f100dde17965a066765fc1c1cb25fb9ea2d33efd
kavaldeep <49375690+kavaldeep@users.noreply.github.com>
2021-02-07 22:15:28 +0530

    app

--- Documents/node/androidTaskManager/app.js
+++ Documents/node/androidTaskManager/app.js
@@ -5,18 +5,10 @@ const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-app.get('/', (req  , res ) => {
+app.get('/start', (req  , res ) => {
   res.send("Welcome Here")
 })
 
-
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
