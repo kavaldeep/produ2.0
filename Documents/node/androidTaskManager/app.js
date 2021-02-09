f7da7eb9ee98ac79d88765d9361ce4d71918e1f1
kavaldeep <49375690+kavaldeep@users.noreply.github.com>
2021-02-09 10:15:53 +0530

    morning commit

--- Documents/node/androidTaskManager/app.js
+++ Documents/node/androidTaskManager/app.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const express = require('express');
 const app = express()
+const bodyParser  = require('body-parser');
 const mongoose = require('./utilis/mongoose')
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/start', (req  , res ) => {
   res.send("Welcome Here")
@@ -18,6 +23,8 @@ app.post('/create' , (req , res) =>{
   res.send(req.body)
   console.log(req.body)
 })
+
+
 
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
